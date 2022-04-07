c4c437d1c4ea1c91a233773696ac560574a5b1b5
MartinTous <martint24.06@gmail.com>
2022-04-07 15:45:42 -0300

    .

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,10 +1,8 @@
-#||============================================================================================||
-#|| 1.Dado un almacén con un layout similar al siguiente, calcular el camino más corto (y la   ||
-#||   distancia) entre 2 posiciones del almacén, dadas las coordenadas de estas posiciones,    ||
-#||   utilizando el algoritmo A*                                                               ||
-#||============================================================================================||
+
 import numpy as np
-#====== Armar la matriz que representa todo el almacen=====#
+import os
+
+#====================== Armar la matriz que representa todo el almacen==================#
 def almacen(matriz):
     PF=0             #Pasillo filas
     PC=0             #Pasillos columnas
@@ -30,7 +28,8 @@ def almacen(matriz):
             PF=0
     return matriz
 
-#============ Ubicar posiciones de destino ================#
+#============================= Ubicar posiciones de destino =============================#
+# Devuelve las coordenadas de la estantería que solicito
 def ubicacion(matriz,pos):
     for i in range(0,10):
         for j in range(0,10):
@@ -38,40 +37,40 @@ def ubicacion(matriz,pos):
                 pos=[i,j]
     return pos
 
-#=================== Algoritmo A* =========================#
+#================================== Algoritmo A* =========================================#
 def Astar(matriz,actual,destino):
     dim=10
     flag=True
 
-    Mfn=[]
+    Mfn=[]                                                                                 #Matriz que guardará las funciones f(n)
     for i in range(0,dim):
         Mfn.append([10000]*dim)
 
-    matriz[actual[0]][actual[1]]='I'
+    matriz[actual[0]][actual[1]]='I'                                                       #Marcar inicio y fin del recorrido en el almacén
     matriz[destino[0]][destino[1]]='F'
     while(flag):
         for i in range(-1,2):
             for j in range(-1,2):
-                if ((i==0 and j==-1)or(i==0 and j==1)or(j==0 and i==-1)or(j==0 and i==1)):
-                    pSig=[actual[0]+i,actual[1]+j]
-                    if pSig[0]>=0 and pSig[1]>=0 and pSig[0]<dim and pSig[1]<dim:
-                        if matriz[pSig[0]][pSig[1]]==0:
-                            f=fn(pSig,destino,actual)
-                            Mfn[pSig[0]][pSig[1]]=f
-                            if f==2:
+                if ((i==0 and j==-1)or(i==0 and j==1)or(j==0 and i==-1)or(j==0 and i==1)): #_Expando el nodo solo hacia arriba, abajo, der e iz
+                    pSig=[actual[0]+i,actual[1]+j]                                         #_Posible posición siguiente. Dependendiendo de su f(n)
+                    if pSig[0]>=0 and pSig[1]>=0 and pSig[0]<dim and pSig[1]<dim:          #_Que no se salga de los límites del almacén
+                        if matriz[pSig[0]][pSig[1]]==0:                                    #_Para que avance solo por los pasillos
+                            f=fn(pSig,destino,actual)                                      #_Calculo f(n) de esa posición
+                            Mfn[pSig[0]][pSig[1]]=f                                        #_Lo almaceno
+                            if f==2:                                                       #_Si estoy a un paso del destino se detiene
                                 flag=False
 
-        Mfn[actual[0]][actual[1]]=10000                 
+        Mfn[actual[0]][actual[1]]=10000                                                    #_Reemplazo el valor de f(n) anterior para no volver a pasar por ese nodo        
         for i in range(0,dim):
             for j in range(0,dim):
-                if (Mfn[i][j])==np.amin(Mfn):
+                if (Mfn[i][j])==np.amin(Mfn):                                              #_Busco el nodo con el menor f(n)
                     actual=[i,j]
            
-        matriz[actual[0]][actual[1]]='#' 
+        matriz[actual[0]][actual[1]]='#'                                                   #Marco el camino en el almacén
    
     return matriz
 
-#=============== Calcular F(n)=h(n)+c(n)===================#
+#==================================== Calcular F(n)=h(n)+c(n)=============================#
 def fn(pSig,destino,Actual): 
     hn=abs(destino[0]-pSig[0])+abs(destino[1]-pSig[1])
     gn=abs(pSig[0]-Actual[0])+abs(pSig[1]-Actual[1])
@@ -79,21 +78,26 @@ def fn(pSig,destino,Actual):
     return fn
 
 
-#=============== MAIN ===================#
+#======================================== MAIN ===========================================#
 if __name__=="__main__":
 
     matriz=[]
-    matriz=almacen(matriz)                                    #Armamos la matriz que representa al almacen
-
-    pos0=[0,0]
-    pos1=ubicacion(matriz,int(input('Posición 1: ')))        # Saber el lugar de la matriz de las posiciones de destino
-  
-
-    posA=pos0                                                # Partimos de las posición cero
-    matriz=Astar(matriz,posA,pos1)                           # Encontramos el camino óptimo
-    
+    matriz=almacen(matriz)                                                                #_Armo la matriz que representa al almacen
     print('\n')
-    for i in range(0,10):
+    for i in range(0,10):                                                                 #_Graficar el almacén y el recorrido
+        for j in range(0,10):
+            print(matriz[i][j],end=' ')
+        print('\n')
+
+    pos0=[0,0]                                                                            #_Punto de partida
+    pos1=ubicacion(matriz,int(input('Posición 1: ')))                                     #_Saber el lugar de la matriz de las posiciones de destino
+  
+    posA=pos0                                                                             #_Partimos de las posición cero
+    matriz=Astar(matriz,posA,pos1)                                                        #_Encontramos el camino óptimo
+
+    os.system('cls')
+    print('\n')
+    for i in range(0,10):                                                                 #_Graficar el almacén y el recorrido
         for j in range(0,10):
             print(matriz[i][j],end=' ')
         print('\n')
