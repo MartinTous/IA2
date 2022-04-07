5d4137de6baa1ef49094655c23955e7d5e9bde4c
MartinTous <martint24.06@gmail.com>
2022-04-07 15:25:28 -0300

    .

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -3,7 +3,7 @@
 #||   distancia) entre 2 posiciones del almacén, dadas las coordenadas de estas posiciones,    ||
 #||   utilizando el algoritmo A*                                                               ||
 #||============================================================================================||
-
+import numpy as np
 #====== Armar la matriz que representa todo el almacen=====#
 def almacen(matriz):
     PF=0             #Pasillo filas
@@ -38,28 +38,43 @@ def ubicacion(matriz,pos):
                 pos=[i,j]
     return pos
 
-#=================== Algoritmo A* 
+#=================== Algoritmo A* =========================#
 def Astar(matriz,actual,destino):
-    fnMenor=1000
-    while(actual!=destino):
+    dim=10
+    flag=True
+
+    Mfn=[]
+    for i in range(0,dim):
+        Mfn.append([10000]*dim)
+
+    matriz[actual[0]][actual[1]]='I'
+    matriz[destino[0]][destino[1]]='F'
+    while(flag):
         for i in range(-1,2):
             for j in range(-1,2):
-                if i == 0 and j ==0:
-                    pass
-                else:
-                    posSig=[actual[0]+i,actual[1]+j]
-                    f=fn(posSig,destino,actual)
-                    if f<fnMenor:
-                        fnMenor=f
-                        PS=posSig
-        actual=PS
-        matriz[PS[0]][PS[1]]='#'
+                if ((i==0 and j==-1)or(i==0 and j==1)or(j==0 and i==-1)or(j==0 and i==1)):
+                    pSig=[actual[0]+i,actual[1]+j]
+                    if pSig[0]>=0 and pSig[1]>=0 and pSig[0]<dim and pSig[1]<dim:
+                        if matriz[pSig[0]][pSig[1]]==0:
+                            f=fn(pSig,destino,actual)
+                            Mfn[pSig[0]][pSig[1]]=f
+                            if f==2:
+                                flag=False
+
+        Mfn[actual[0]][actual[1]]=10000                 
+        for i in range(0,dim):
+            for j in range(0,dim):
+                if (Mfn[i][j])==np.amin(Mfn):
+                    actual=[i,j]
+           
+        matriz[actual[0]][actual[1]]='#' 
+   
     return matriz
 
 #=============== Calcular F(n)=h(n)+c(n)===================#
-def fn(posSig,pos1,posA): 
-    hn=abs(pos1[0]-posSig[0])+abs(pos1[1]-posSig[1])
-    gn=abs(posSig[0]-posA[0])+abs(posSig[1]-posA[1])
+def fn(pSig,destino,Actual): 
+    hn=abs(destino[0]-pSig[0])+abs(destino[1]-pSig[1])
+    gn=abs(pSig[0]-Actual[0])+abs(pSig[1]-Actual[1])
     fn=hn+gn
     return fn
 
@@ -70,14 +85,15 @@ if __name__=="__main__":
     matriz=[]
     matriz=almacen(matriz)                                    #Armamos la matriz que representa al almacen
 
-    pos0=[1,1]
+    pos0=[0,0]
     pos1=ubicacion(matriz,int(input('Posición 1: ')))        # Saber el lugar de la matriz de las posiciones de destino
-    pos2=ubicacion(matriz,int(input('Posición 1: ')))   
+  
 
     posA=pos0                                                # Partimos de las posición cero
     matriz=Astar(matriz,posA,pos1)                           # Encontramos el camino óptimo
-
+    
+    print('\n')
     for i in range(0,10):
-        print(matriz[i][:])
-
-# final
+        for j in range(0,10):
+            print(matriz[i][j],end=' ')
+        print('\n')
