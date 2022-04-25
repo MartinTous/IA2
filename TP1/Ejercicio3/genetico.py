559d3e2cdf10d588f5cd904fbd8fc229ff807f60
GabrielCabreroGarcia <gabrielcabrero28@gmail.com>
2022-04-25 16:00:44 -0300

    .

--- TP1/Ejercicio3/genetico.py
+++ TP1/Ejercicio3/genetico.py
@@ -1,41 +1,32 @@
 from random import random
 
 class individuo():
-    def __init__(self,list):
+    def __init__(self,list,ordenes):
         # individuo
         self.disposicion = list
         # Fitness del individuo
-        self.fitness = self.setfitness()
-    def setfitness():
+        self.fitness = self.setfitness(ordenes)
+    def setfitness(self,ordenes):
         #calculo el fitness con recocido simulado
-        pass   
+        fitness = 0
+        for i in range (len(ordenes)):
+            nf = recocido_simulado(self.disposicion,ordenes)
+            fitness = fitness + nf
+        return fitness
+          
 
 
-def genetico (disp):
+def genetico (disp,ordenes):
     poblacion = []
-    for i in range(len(disp)):
-        poblacion[i] = individuo(disp(i))
-        
-
     for j in range(1000):
-        poblacion = seleccion(poblacion)
+        for i in range(len(disp)):
+            poblacion[i] = individuo(disp(i),ordenes)
         
-        for i in range(len(poblacion)):
-            a = random(0,len()) 
-            b = random(0,len())
-            c = random(0,len())
+        poblacion = seleccion(poblacion)
+        poblacion = crossover(poblacion)
+        poblacion = mutacion(poblacion)
 
-            a1 = poblacion [i][a]
-            a2 = poblacion [i][b]
-            a3 = poblacion [i][c]
 
-            poblacion [i][a] = a2
-            poblacion [i][b] = a3
-            poblacion [i][c] = a1
-
-            poblacion [i + len(poblacion)][a] = a3
-            poblacion [i + len(poblacion) + 1][b] = a1
-            poblacion [i + len(poblacion) + 2][c] = a2
 
 
     poblacion = seleccion(poblacion)
@@ -44,8 +35,42 @@ def genetico (disp):
 
     print(pobok)
 
+
+
+
+
+
+
+
 def seleccion(poblacion):
-    sorted(poblacion, key=lambda x: x.fitness)
+    sorted(poblacion, key=lambda poblacion: poblacion.fitness)
     for i in range(len(poblacion)/2):
         poblacion [i] = poblacion [i]
+    return poblacion
+
+
+def crossover(lista): 
+  
+    k = random.randint(0, len(lista[1]),2) 
+
+    for i in range (len(lista)):
+        for j in range(k,len(lista[i])): 
+            lista[i][j], lista[i+1][j] = lista[i+1][j], lista[i][j]   
+      
+    return lista 
+
+def mutacion (poblacion):
+    for i in range(len(poblacion)):
+        a = random.randint(0,len(poblacion[i])) 
+        b = random.randint(0,len(poblacion[i]))
+        c = random.randint(0,len(poblacion[i]))
+
+        a1 = poblacion [i][a]
+        a2 = poblacion [i][b]
+        a3 = poblacion [i][c]
+
+        poblacion [i][a] = a2
+        poblacion [i][b] = a3
+        poblacion [i][c] = a1
+    
     return poblacion
